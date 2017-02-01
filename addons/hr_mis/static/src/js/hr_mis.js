e5b2c6934f5d37aa3b372c683f34b922164def73
Kostik <kostik@kostik.net>
2017-01-31 18:53:06 -0800

    fixes #510 - Imperial values entering

--- addons/hr_mis/static/src/js/hr_mis.js
+++ addons/hr_mis/static/src/js/hr_mis.js
@@ -9,6 +9,8 @@ odoo.define('web.web_widget_imperial_length', function (require) {
     var FormView = require('web.FormView');
 
     var QWeb = core.qweb;
+    var cm_per_foot = 30.48;
+    var cm_per_inch = 2.54;
 
     var ImperialLength = widget.FieldChar.extend({
 
@@ -28,6 +30,15 @@ odoo.define('web.web_widget_imperial_length', function (require) {
             var $input = this.$('input');
             if (!this.get("effective_readonly") && $input.size() > 0) {
                 var value = $input.val();
+                if (!value) {
+                    return true;
+                }
+                var re = /^(\d+)\s+(\d+(\.\d{1,2}))?$/; // i.e. 6 2.5
+                var values;
+                if ((values = re.exec(value)) !== null) {
+                    value = parseInt(values[1]*cm_per_foot + values[2]*cm_per_inch);
+                    $input.val(value);
+                }
                 return !isNaN(value) &&
                     parseInt(Number(value)) == value && !isNaN(parseInt(value, 10));
             }
@@ -46,7 +57,7 @@ odoo.define('web.web_widget_imperial_length', function (require) {
         render_value: function () {
             if (this.get("effective_readonly")) {
                 var val = this.get("value");
-                var feet = val / 30.48;
+                var feet = val / cm_per_foot;
                 this.$el.text(val + " cm (" + Math.floor(feet) + " ft " + Math.round((feet % 1) * 120) / 10 + " in)");
             } else {
                 this.$("input").val(this.get("value"));
